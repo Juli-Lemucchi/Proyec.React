f2ba2f6279edb4b9fe894af38597705d9b43a97c
juLemucchi <julietanatalia2004@gmail.com>
2024-04-23 21:11:36 -0300

    .

--- entregaReact-Lemucchi/src/componentes/pages/Libros.jsx
+++ entregaReact-Lemucchi/src/componentes/pages/Libros.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import CardLibrosContainer from '../subcomponentes/CardLibrosContainer'
+import { Link } from "react-router-dom"
+function Libros (libros) {
 
-function Libros() {
+  const {portada,title,precio,id} = libros
+
   return (
-    <div>
-      <h2>Nuestros libros</h2>
-      <section className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3'>
-      <CardLibrosContainer/>
-      </section>
-      
+    <div className="m-2 flex flex-col items-center border-2 solid rounded-xl">
+        <div><img src={portada} alt={title} className="w-48 h-70 object-cover p-3" /></div>
+        <h2 className="text-center text-2xl font-bold p-4 "> {title} </h2>
+        <div className="mt-2 gap-2 flex flex-row items-center md:flex-row md:justify-between">
+          <h3>{precio}</h3>
+          <Link className="bg-red-400 w-14 h-6 rounded-md font-serif text-sm text-blak" to={`/Libros/${id}`} >
+            <button>Ver mas</button></Link>
+        </div>
     </div>
   )
 }
--- entregaReact-Lemucchi/src/componentes/subcomponentes/CardLibrosContainer.jsx
+++ entregaReact-Lemucchi/src/componentes/subcomponentes/CardLibrosContainer.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom"
+import Libros from "./pages./Libros.jsx";
+
 function CardLibrosContainer() {
       
 
@@ -16,37 +18,23 @@ useEffect(() => {
     pedido.fetch("./data.json")
   }
   pedido.then((response) => {
-
-      if (!response.ok) {
-      throw new Error("Error al cargar los libros");
-      }
       return response.json();
     })
-
-    pedido.then((data) => {
+  pedido.then((data) => {
       setLibros(data);
     })
-
-    pedido.catch((error) => {
+  pedido.catch((error) => {
     console.error("Error busqueda libros:", error);
     });
   
   }, [params.categoria]);
-
+  console.log(libros)
 return (
 
-<> {libros.map((libros) => (
-   <div key={libros.id} className="m-2 flex flex-col items-center border-2 solid rounded-xl">
-     <div><img src={libros.portada} alt={libros.title} className="w-48 h-70 object-cover p-3" /></div>
-     <h2 className="text-center text-2xl font-bold p-4 "> {libros.title} </h2>
-      <div className="mt-2 gap-2 flex flex-row items-center md:flex-row md:justify-between">
-          <h3>{libros.precio}</h3>
-          <Link className="bg-red-400 w-14 h-6 rounded-md font-serif text-sm text-blak" to={`/Libros/${libros.id}`} >
-            <button>Ver mas</button></Link>
-      </div>
-  </div>
-  ))}
-</>
+<> {libros.map((libros) => {
+      return <Libros key={libros.id} libros={libros} />
+  })}
+  </>
 )}
 
 
