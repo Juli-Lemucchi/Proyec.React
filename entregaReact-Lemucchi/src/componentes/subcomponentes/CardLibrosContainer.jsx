d586de99b8b21c5b5dbcfc0331a2f321fdd0a826
juLemucchi <julietanatalia2004@gmail.com>
2024-05-14 14:55:56 -0300

    .

--- entregaReact-Lemucchi/src/componentes/subcomponentes/CardLibrosContainer.jsx
+++ entregaReact-Lemucchi/src/componentes/subcomponentes/CardLibrosContainer.jsx
@@ -2,13 +2,20 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom"
 import Libros from "../pages/Libros";
 import data from "../data.json";
+import {app} from"../../firebase"
+import { collection, getFirestore} from "firebase/firestore";
+
+
+const infoFirebase = getFirestore(app)
+
+const coleccionLibros =collection(infoFirebase, "libros")
 
 function CardLibrosContainer() {
       
 
     const [libros, setLibros] = useState([]);
     const category = useParams().categoria 
-
+    
 
     const pedido = () => {
       return new Promise((resolve, reject) => {
---
+++ entregaReact-Lemucchi/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBajlWBsrhGliJfTvz8KD-2oKt73-Lhleg",
+  authDomain: "dinobooks-1aea8.firebaseapp.com",
+  projectId: "dinobooks-1aea8",
+  storageBucket: "dinobooks-1aea8.appspot.com",
+  messagingSenderId: "71868585726",
+  appId: "1:71868585726:web:3167eac290e946f1dbf7af"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
